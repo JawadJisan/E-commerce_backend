ba469cbc78dbaad979e24203905fe119ed37c9f1
zahidmiran <zahidmiron599@gmail.com>
2023-11-25 22:27:08 +0600

    backedn server found

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-const errorMiddleware = require('./middleware/error')
-
+const errorMiddleware = require("./middleware/error");
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    dotenv.config({ path: "backend/.env" });
+  dotenv.config({ path: "backend/.env" });
 }
 
+// middlewares
 // middlewares
 app.use(express.json());
 app.use(cors());
@@ -20,10 +20,9 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-
 //Route import
-app.get('/', (req, res) => {
-    res.send('Route is working! YaY!');
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
 });
 
 const product = require("./routes/productRoute");
@@ -31,15 +30,12 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
-
-
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 // middleware for error
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-
-module.exports = app
+module.exports = app;
